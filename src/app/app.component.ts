3979591878771b90a696a8cccf2b1d1e29cf5cc3
Rajat Verma <138663302+rajat-synergy@users.noreply.github.com>
2024-08-22 17:10:28 +0530

    add details page and add new slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,6 +9,42 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'arti_nagada';
+  cards = [
+    { image: 'assets/images/client-1.png', title: 'Dilip Panwar', description: 'I did lot of research before purchasing the product and realised that Vishwakarma is one of the best manufacturer for Automatic Nagada Machine. Their delivery process is also very smooth and robust. Would like to deal again in future' },
+    { image: 'assets/images/client-2.png', title: 'Jon', description: 'I did lot of research before purchasing the product and realised that Vishwakarma is one of the best manufacturer for Automatic Nagada Machine. Their delivery process is also very smooth and robust. Would like to deal again in future' },
+    { image: 'assets/images/client-1.png', title: 'Danial', description: 'I did lot of research before purchasing the product and realised that Vishwakarma is one of the best manufacturer for Automatic Nagada Machine. Their delivery process is also very smooth and robust. Would like to deal again in future' },
+    { image: 'assets/images/client-2.png', title: 'ben', description: 'I did lot of research before purchasing the product and realised that Vishwakarma is one of the best manufacturer for Automatic Nagada Machine. Their delivery process is also very smooth and robust. Would like to deal again in future' },
+    { image: 'assets/images/client-1.png', title: 'krish', description: 'I did lot of research before purchasing the product and realised that Vishwakarma is one of the best manufacturer for Automatic Nagada Machine. Their delivery process is also very smooth and robust. Would like to deal again in future' },
+    { image: 'assets/images/client-2.png', title: 'taler', description: 'I did lot of research before purchasing the product and realised that Vishwakarma is one of the best manufacturer for Automatic Nagada Machine. Their delivery process is also very smooth and robust. Would like to deal again in future' }
+    // Add more cards as needed
+  ];
+
+  currentTransform = 0;
+  visibleCards = 3; // Adjust the number of visible cards
+
+  get maxTransform() {
+    return -(this.cards.length - this.visibleCards) * (100 / this.visibleCards);
+  }
+
+  ngOnInit() {
+    // Automatically slide every 3 seconds
+    setInterval(() => {
+      this.nextSlide();
+    }, 3000); // Adjust the interval as needed
+  }
+
+  prevSlide() {
+    if (this.currentTransform < 0) {
+      this.currentTransform += 100 / this.visibleCards;
+    }
+  }
+
+  nextSlide() {
+    if (this.currentTransform > this.maxTransform) {
+      this.currentTransform -= 100 / this.visibleCards;
+    }
+  }
 }
+
